f1132bd096f7c9cde0d173028081c7d493aa8803
Cheolyeon Byun <markorea9708@gmail.com>
2024-09-23 07:25:01 +0900

    fix: Config::new is now build, and uses Result instead of just returning Config

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,23 @@ struct Config {
     file_path: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config,&'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments, must provide 3");
+        }
         let search_query = args[1].clone();
         let file_path = args[2].clone();
     
-        Config { search_query, file_path }
+        Ok(Config { search_query, file_path })
     }   
 }
 
 fn main() {
     let command_line_arguments: Vec<String> = env::args().collect();
-    let config = Config::new(&command_line_arguments);
-
+    let config = Config::build(&command_line_arguments);
+    // what is a rust way of ensuring rust dont panic if no argument was provided?
+    // use result type for config?
+    
     let file_content = read_file_content(&config.file_path).expect("failed to read file content!");
     println!("search query is {}",config.search_query);
     println!("file path is {}",config.file_path);
